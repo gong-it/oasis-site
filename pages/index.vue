588bdde37ac3ec8bda10e09fe6f6913be2589461
GONG <cancookbs@gmail.com>
2021-11-08 20:56:29 +0000

    Fix bg width

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h1 class="hero__header" style="color: #AAAAA2;z-index: 1;position: relative; letter-spacing: .25em; padding-top: 6rem; text-align: center">
           Site is under reconstruction
         </h1>
-        <img style="position: absolute; bottom: 0; left: 0; right: 0;" src="/palm.png" alt="palm">
+        <img style="position: absolute; bottom: 0; left: 0; right: 0; width: 100%" src="/palm.png" alt="palm">
       </section>
     </full-page>
   </div>
